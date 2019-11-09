0e1a1fce1644c4874268d71189486958de3d705a
siturbe82 <santiago.iturbe@gmail.com>
2019-11-09 08:50:14 -0600

    This time really fixing that typo

--- models/results.js
+++ models/results.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
         pick1: DataTypes.STRING,
         pick1points: DataTypes.INTEGER,
         pick2: DataTypes.STRING,
-        pick2points: DataTypes.INEGER,
+        pick2points: DataTypes.INTEGER,
         pick3: DataTypes.STRING,
         pick3points: DataTypes.INTEGER,
     });
