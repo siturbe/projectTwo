129dcf85a56f066bb2ebae70e56056ec13b54766
Chris <reducks3000@gmail.com>
2019-11-14 12:56:36 -0600

    added JS to standingsfront.js to stylize users and points

--- public/assets/js/standingsFront.js
+++ public/assets/js/standingsFront.js
@@ -7,8 +7,21 @@ $.get("/api/standings", function(data){
         tableSection.attr("id", "ranking-" + i);
         $("#standingsTable").append(tableSection);
 
-        $('#ranking-' + i).append("<h4>" + data[i].name + " \xa0\xa0\xa0\xa0  " + data[i].points +"</h4>");
+        // $('#ranking-' + i).append("<h4>" + data[i].name + " \xa0\xa0\xa0\xa0  " + data[i].points +"</h4>");
+        
+        let tableRow = $('<tr/>')
+        .appendTo($('#pointsTableBody'))
+
+        let PlayerName = $('<td>' + data[i].name +'</td>')
+        .appendTo(tableRow)
+
+        // let tableSpace = $('<td>' + data[i].id + '</td>')
+        //         .appendTo(tableRow)
+
+        let PlayerPoints = $('<td>' + data[i].points + '<td>')
+        .appendTo(tableRow)
     }
+    
 })
 
 
