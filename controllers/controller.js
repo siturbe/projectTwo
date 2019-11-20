e6194637ecbbe0318b18ffc79229f9e27cd1ad14
siturbe82 <santiago.iturbe@gmail.com>
2019-11-19 18:05:50 -0600

    Tweaking placing of jobs.option() so doesn't have sync issue

--- controllers/controller.js
+++ controllers/controller.js
@@ -35,6 +35,7 @@ module.exports = function(app){
     // })
 
     app.get("/standings", function (req, res){
+        jobs.options();
         res.render(path.join(__dirname, "../views/layouts/standings.pug"));
     })
     
@@ -44,7 +45,7 @@ module.exports = function(app){
 
     //APIs so that user can pick games of the week
     app.get("/pickGames", function(req, res){
-        jobs.options();
+        // jobs.options();
         res.render(path.join(__dirname, "../views/layouts/pickGames.pug"));
     })
 
