c85e2f5cc3c48073bc9405bae8ea25db04531dce
siturbe82 <santiago.iturbe@gmail.com>
2019-11-19 23:44:24 -0600

    Modifying code to see if works with Heroku

--- controllers/controller.js
+++ controllers/controller.js
@@ -45,7 +45,7 @@ module.exports = function(app){
 
     //APIs so that user can pick games of the week
     app.get("/pickGames", function(req, res){
-        // jobs.options();
+        jobs.options();
         res.render(path.join(__dirname, "../views/layouts/pickGames.pug"));
     })
 
@@ -178,9 +178,9 @@ module.exports = function(app){
     });
 
     //route to populate options table on login
-    app.get('/api/populateOptions', function(req, res){
-        jobs.options();
-    })
+    // app.get('/api/populateOptions', function(req, res){
+    //     jobs.options();
+    // })
 
 }
 
--- public/assets/js/populateOptions.js
+++ public/assets/js/populateOptions.js
@@ -1,6 +1,6 @@
-let dataOfOptions
+// let dataOfOptions
 
-$.get('/api/populateOptions', function(data){
-    dataOfOptions = data;
-    console.log('Populated options table');
-})
+// $.get('/api/populateOptions', function(data){
+//     dataOfOptions = data;
+//     console.log('Populated options table');
+// })
