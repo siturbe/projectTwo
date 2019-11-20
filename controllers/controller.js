45f0af7298fce94c9221892bb79391338c6c588a
siturbe82 <santiago.iturbe@gmail.com>
2019-11-19 23:55:37 -0600

    Tried adding await to job.options() in a diff location

--- controllers/controller.js
+++ controllers/controller.js
@@ -44,8 +44,8 @@ module.exports = function(app){
     })
 
     //APIs so that user can pick games of the week
-    app.get("/pickGames", function(req, res){
-        jobs.options();
+    app.get("/pickGames", async function(req, res){
+        await jobs.options();
         res.render(path.join(__dirname, "../views/layouts/pickGames.pug"));
     })
 
